a1c6348e693bb017b137240e91053729e3f90050
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-10-05 19:24:41 -0300

    Crie o endpoint GET /talker/:id

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const fs = require('fs').promises;
 const path = require('path');
+// const { isNumberObject } = require('util/types');
+const talkerData = require('./talker.json');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+const pathTalker = path.resolve(__dirname, '..', 'src', 'talker.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,16 +22,18 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/talker', async (_req, res) => {
-  const pathTalker = path.resolve(__dirname, '..', 'src', 'talker.json');
   const managers = JSON.parse(await fs.readFile(pathTalker, 'utf8'));
-  console.log(managers);
   res.status(200).json(managers);
 });
 
-app.post('/talker', (req, res) => {
-  const managers = { ...req.body };
-  console.log(managers);
-  res.end();
+app.get('/talker/:id', (req, res) => {
+    const idPar = Number(req.params.id);
+    const manager = talkerData.find((e) => e.id === idPar);
+    if (!manager) {
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    } else {
+      res.status(200).json(manager);
+    }
 });
 
 app.listen(PORT, () => {
